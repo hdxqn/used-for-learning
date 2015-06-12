df96d3bd00db2a0d8e50ba3cf0382e6389bcfa4c
hdxqn <314123010@qq.com>
2015-06-12 17:03:34 +0800

    添加node.js学习文件，和i18n多语言实现例子

---
+++ test/scripts/main.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function(){
+	touchEvents={};
+	browserRedirect(touchEvents);
+	i18n.init({
+        "lng": 'en',
+        "resStore": resources,
+        "fallbackLng" : 'en'
+    }, function (t) {
+        $(document).i18n();
+        console.log(t("text"));
+        console.debug(window.navigator.language);
+        explain=function(){
+        	console.debug(t("number"));
+        	var lang=getUrlParam('lang');
+        	console.debug(lang);
+        }
+    });
+    $('body').click(init);
+
+
+});
+function init(){
+	 explain('number');
+	 console.debug('i am clicked!');
+}
+function browserRedirect(obj) {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+         
+    if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+         obj.touchstart="touchstart";
+            obj.touchmove= "touchmove";
+            obj.touchend= "touchend";
+         
+    } else {
+        
+                obj.touchstart = "mousedown";
+                obj.touchmove = "mousemove";
+                obj.touchend = "mouseup";
+         
+    }
+ }
+function getUrlParam(e) {
+    var t = new RegExp("(^|&)" + e + "=([^&]*)(&|$)"),
+        n = window.location.search.substr(1).match(t);
+    return null != n ? unescape(n[2]) : null
+}
+
+
---
+++ test/scripts/resources.js
@@ -0,0 +1,14 @@
+var resources={
+	"ch":{
+		"translation":{
+			"text":"测试文本",
+			"number":"测试数字"
+		}
+	},
+	"en":{
+		"translation":{
+			"text":"test words",
+			"number":"test nums"
+		}
+	}
+};
