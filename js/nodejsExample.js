051e0f37a6adfd49a4a91e1a5ddd8cb50d190a78
lijianshi <hdxqnljs@163.com>
2016-04-22 18:16:35 +0800

    collecting new code

--- js/nodejsExample.js
+++ js/nodejsExample.js
@@ -1,14 +1,24 @@
-const http = require('http');
+//const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//const hostname = '127.0.0.1';
+//const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-})
+//const server = http.createServer((req, res) => {
+  //res.statusCode = 200;
+  //res.setHeader('Content-Type', 'text/plain');
+  //res.end('Hello World\n');
+//})
 
-server.listen(port, hostname, () => {
-  console.log("Server running at http://${hostname}:${port}/");
+//server.listen(port, hostname, () => {
+  //console.log("Server running at http://${hostname}:${port}/");
+//});
+const fs=require("fs");
+
+fs.readFile("nodeText.text",function(err,data){
+	if(err){
+	  return console.error(err);
+	}
+	console.log(data.toString());
 });
+
+console.log("程序执行结束！");
