a7a9c229adf27fc9499b9e429de221dfc23d3c02
lijianshi <hdxqnljs@163.com>
2016-04-25 17:45:52 +0800

    添加button页面和滑落效果页面

--- js/nodejsExample.js
+++ js/nodejsExample.js
@@ -12,13 +12,23 @@
 //server.listen(port, hostname, () => {
   //console.log("Server running at http://${hostname}:${port}/");
 //});
-const fs=require("fs");
 
-fs.readFile("nodeText.text",function(err,data){
-	if(err){
-	  return console.error(err);
-	}
-	console.log(data.toString());
-});
+//es6的对象合并方法assign,可以很方便的把字符转变为类数组对象
 
-console.log("程序执行结束！");
+  String.prototype.deleteBlank=deleteBlank;	 
+  var str="hello world my friends";
+  console.log(Object.assign({},str.deleteBlank()));//首个参数不能为undefined 和null
+  
+  function deleteBlank(){
+  	return this.replace(/\s/g,"");
+  }
+//const fs=require("fs");
+
+//fs.readFile("nodeText.text",(err,data)=>{
+//	if(err){
+//	  return console.error(err);
+//	}
+//	console.log(data.toString());
+//});
+
+//console.log("程序执行结束！");
